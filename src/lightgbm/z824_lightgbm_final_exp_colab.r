2b0c250e90601d386251c4b287c5889bcc6ca680
telsesser <tomaselsesser@gmail.com>
2023-11-13 21:05:34 -0300

    asdf

--- src/lightgbm/z824_lightgbm_final_exp_colab.r
+++ src/lightgbm/z824_lightgbm_final_exp_colab.r
@@ -15,12 +15,32 @@ require("lightgbm")
 #  muy pronto esto se leera desde un archivo formato .yaml
 
 PARAM <- list()
-PARAM$experimento <- "KA8240"
+PARAM$experimento <- "Exp_col"
 # setwd("/home/aleb/dmeyf23/datasets")
-PARAM$input$dataset <- "./datasets/competencia_03_exp_col.csv.gz"
+PARAM$input$dataset0 <- "./datasets/competencia_03_exp_col.csv.gz"
+PARAM$input$dataset1 <- "./datasets/competencia_03_exp_col_lags_1.csv.gz"
+PARAM$input$dataset2 <- "./datasets/competencia_03_exp_col_lags_2.csv.gz"
+
+setwd("~/buckets/b1")
+dataset0 <- fread(PARAM$input$dataset0, stringsAsFactors = TRUE)
+dataset1 <- fread(PARAM$input$dataset1, stringsAsFactors = TRUE)
+dataset2 <- fread(PARAM$input$dataset2, stringsAsFactors = TRUE)
+
+dataset <- cbind(dataset0, dataset1, dataset2)
+
+# Obtener los nombres de las columnas del dataset
+nombres_columnas <- colnames(dataset)
+
+lista_ct_0 <- nombres_columnas[grep("^ct_0_|^lag_ct_0_", nombres_columnas)]
+lista_ct_it <- nombres_columnas[grep("^ct_it_|^lag_ct_it_", nombres_columnas)]
+lista_ct_median_6m <- nombres_columnas[grep("^ct_median_6m_|^lag_ct_median_6m_", nombres_columnas)]
+lista_ct_mean_6m <- nombres_columnas[grep("^ct_mean_6m|^lag_ct_mean_6m", nombres_columnas)]
+lista_ct_median <- nombres_columnas[grep("^ct_median|^lag_ct_median", nombres_columnas)]
+lista_ct_mean <- nombres_columnas[grep("^ct_mean|^lag_ct_mean", nombres_columnas)]
+
 
 # meses donde se entrena el modelo
-PARAM$input$training <- c(202102, 202103, 202104, 202105)
+PARAM$input$training <- c(201905, 201908, 201910, 202006, 202012, 202106)
 PARAM$input$future <- c(202107) # meses donde se aplica el modelo
 
 PARAM$finalmodel$semilla <- 135977
@@ -64,136 +84,101 @@ PARAM$finalmodel$lgb_basicos <- list(
 )
 
 
-#------------------------------------------------------------------------------
-#------------------------------------------------------------------------------
-dataset <- fread(PARAM$input$dataset, stringsAsFactors = TRUE)
-
-
-# Catastrophe Analysis  -------------------------------------------------------
-# deben ir cosas de este estilo
-#   dataset[foto_mes == 202006, active_quarter := NA]
-
-# Data Drifting
-# por ahora, no hago nada
-
-
-# Feature Engineering Historico  ----------------------------------------------
-#   aqui deben calcularse los  lags y  lag_delta
-#   Sin lags no hay paraiso ! corta la bocha
-#   https://rdrr.io/cran/data.table/man/shift.html
-
-
-#--------------------------------------
-
-# paso la clase a binaria que tome valores {0,1}  enteros
-# set trabaja con la clase  POS = { BAJA+1, BAJA+2 }
-# esta estrategia es MUY importante
 dataset[, clase01 := ifelse(clase_ternaria %in% c("BAJA+2", "BAJA+1"), 1L, 0L)]
 
 #--------------------------------------
-
-# los campos que se van a utilizar
-campos_buenos <- setdiff(colnames(dataset), c("clase_ternaria", "clase01"))
-
-#--------------------------------------
+nombres_columnas <- colnames(dataset)
 
 
-# establezco donde entreno
+columnas_ct_lag <- nombres_columnas[grep("^ct_|^lag_ct", nombres_columnas)]
+columnas_no_ct_lag <- setdiff(nombres_columnas, columnas_ct_lag)
+
+lista_no_train <- c("clase_ternaria", "clase01", "(mes + 1)", "(mes + 2)", "foto_mes", "mes")
+
+campos_genericos <- setdiff(columnas_no_ct_lag, lista_no_train)
+
+# Filtrar las columnas que comienzan con "ct_0"
+columnas_ct_0 <- nombres_columnas[grep("^ct_0_", nombres_columnas)]
+columnas_sin_prefijo <- sub("^ct_0_", "", columnas_ct_0)
+columnas_ct_0 <- nombres_columnas[grep("^ct_0_", nombres_columnas)]
+
+
+
 dapply <- dataset[foto_mes %in% PARAM$input$future]
 dataset <- dataset[foto_mes %in% PARAM$input$training]
 gc() # garbage collection
+# Iniciar el ciclo para recorrer diferentes valores de i
+resultados <- data.table()
 
+datasets <- c("lista_ct_0", "lista_ct_it", "lista_ct_median_6m", "lista_ct_mean_6m", "lista_ct_median", "lista_ct_mean")
+setwd(paste0("./exp/", PARAM$experimento, "/"))
+for (j in datasets) {
+  columnas_asignadas <- get(j)
+  print(paste0("dataset ", j, " con ", length(columnas_asignadas), " columnas y ", nrow(dataset), " filas"))
+  campos_buenos <- Reduce(union, c(
+    columnas_asignadas,
+    campos_genericos
+  ))
 
-
-
-# dejo los datos en el formato que necesita LightGBM
-dtrain <- lgb.Dataset(
-  data = data.matrix(dataset[, campos_buenos, with = FALSE]),
-  label = dataset[, clase01]
-)
-gc() # garbage collection
-
-# genero el modelo
-param_completo <- c(
-  PARAM$finalmodel$lgb_basicos,
-  PARAM$finalmodel$optim
-)
-
-# create an empty list to store the models
-models <- list()
-
-seeds <- c(2, 17, 31, 53, 73, 97)
-
-# define a function to build a model with a given seed
-build_model <- function(seed) {
-  modelo <- lgb.train(
-    data = dtrain,
-    param = c(param_completo, seed = seed)
+  dtrain <- lgb.Dataset(
+    data = data.matrix(dataset[, campos_buenos, with = FALSE]),
+    label = dataset[, clase01]
   )
-  print(paste0("Modelo con semilla ", seed, " terminado"))
   gc()
-  return(modelo)
-}
 
-# use lapply to build a list of models with different seeds
-models <- lapply(seeds, build_model)
-
-# aplico el modelo a los datos sin clase
-
-
-# create an empty list to store the predictions
-predictions <- list()
-
-for (i in seq_along(models)) {
-  pred <- predict(
-    models[[i]],
-    data.matrix(dapply[, campos_buenos, with = FALSE])
+  param_completo <- c(
+    PARAM$finalmodel$lgb_basicos,
+    PARAM$finalmodel$optim
   )
-  predictions <- c(predictions, list(pred))
-}
 
-# take the mean of the predictions across all models
-row_means <- rowMeans(do.call(cbind, predictions))
-row_means[1:20]
-predictions <- c(predictions, list(row_means))
+  # create an empty list to store the models
+  models <- list()
 
-for (i in seq_along(predictions)) {
-  print(predictions[[i]][1])
-}
-# genero la tabla de entrega
-tb_entrega <- dapply[, list(numero_de_cliente, foto_mes, clase_ternaria)]
-tb_entrega[, prob := prediccion]
+  seeds <- c(2, 104147, 41947, 59809, 70507, 82073, 15649, 28403)
 
-for (p in predictions) {
+  # define a function to build a model with a given seed
+  build_model <- function(seed) {
+    modelo <- lgb.train(
+      data = dtrain,
+      param = c(param_completo, seed = seed)
+    )
+    print(paste0("Modelo con semilla ", seed, " terminado"))
+    gc()
+    return(modelo)
+  }
+
+  # use lapply to build a list of models with different seeds
+  models <- lapply(seeds, build_model)
+
+  # aplico el modelo a los datos sin clase
+
+  predictions <- list()
+  for (i in seq_along(models)) {
+    pred <- predict(
+      models[[i]],
+      data.matrix(dapply[, campos_buenos, with = FALSE])
+    )
+
+    print(paste0("Modelo con semilla ", i, " terminado"))
+    predictions <- c(predictions, list(pred))
+  }
+
+  # take the mean of the predictions across all models
+  row_means <- rowMeans(do.call(cbind, predictions))
+  predictions <- c(predictions, list(row_means))
+
+
+  # genero la tabla de entrega
+  scores <- c()
   tb_entrega <- dapply[, list(numero_de_cliente, foto_mes, clase_ternaria)]
-  tb_entrega[, prob := p]
-  setorder(tb_entrega, -prob)
-  tb_entrega[, Predicted := 0L]
-  tb_entrega[1:15000, Predicted := 1L]
-  score <- sum(tb_entrega$Predicted * ifelse(tb_entrega$clase_ternaria == "BAJA+2", 273000, -7000))
-  print(score)
+  for (i in seq_along(predictions)) {
+    col_name <- paste0("pred_", i)
+    # tb_entrega[, (col_name) := predictions[[i]]]
+    tb_entrega[, (col_name) := ifelse(predictions[[i]] > 0.025, 1L, 0L)]
+    score <- sum(tb_entrega[, (col_name), with = FALSE] * ifelse(tb_entrega$clase_ternaria == "BAJA+2", 273000L, -7000L))
+    # score <- tb_entrega[, sum(.SD * ifelse(clase_ternaria == "BAJA+2", 273000, -7000)), .SDcols = patterns("^pred_")]
+    scores <- c(scores, score)
+    print(paste("dataset ", j, ": ", col_name, " - score: ", score))
+  }
+  fwrite(tb_entrega, paste0("tb_entrega_", j, ".csv.gz"), row.names = FALSE, compression = "gzip")
 }
-
-
-
-
-
-# tb_entrega[, Predicted := ifelse(prob > 0.025, 1L, 0L)]
-cortes <- seq(000, 10000, by = 1)
-scores <- c()
-for (envios in cortes) {
-  tb_entrega[, Predicted := 0L]
-  tb_entrega[1:envios, Predicted := 1L]
-  score <- sum(tb_entrega$Predicted * ifelse(tb_entrega$clase_ternaria == "BAJA+2", 273000, -7000))
-  print(score)
-  scores <- c(scores, score)
-}
-plot(cortes, scores, type = "l", xlab = "Corte", ylab = "Score")
-fwrite(tb_entrega[, list(numero_de_cliente, clase_ternaria)],
-  file = paste0(PARAM$experimento, "_", sum(tb_entrega$Predicted), ".csv"),
-  sep = ","
-)
-
-sum(dataset$foto_mes == PARAM$input$future)
-
-cat("\n\nLa generacion de los archivos para Kaggle ha terminado\n")
